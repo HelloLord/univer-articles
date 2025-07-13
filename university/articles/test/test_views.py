11b055402a3de0cb86b74224c8a7c82961e928ee
ilya <ilyagodvin@gmail.com>
2025-07-13 17:27:03 +0300

    v2.3

--- university/articles/test/test_views.py
+++ university/articles/test/test_views.py
@@ -1,33 +1,9 @@
-from audioop import reverse
 
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 
 CustomUser = get_user_model()
-
-class TestRegisterCase(APITestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.register_url = '/register'
-        self.valid_payload = {
-            'username': 'testuser',
-            'password': 'testpass123',
-            'email': 'test@example.com',
-        }
-    def test_create_user_with_valid_data(self):
-        response = self.client.post(
-            self.register_url,
-            data=self.valid_payload,
-            format='json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_302_FOUND)
-        self.assertTrue(CustomUser.objects.filter(username='testuser').exists())
-
-        user = CustomUser.objects.get(username = 'testuser')
-        self.assertTrue(user.is_authenticated)
-
-
 
 class BaseTestCase(APITestCase):
     TEST_CONTENT = {
