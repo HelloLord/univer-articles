e02125da814cf884ee5b4adda68b914daa2f5284
ilya <ilyagodvin@gmail.com>
2025-07-23 20:03:59 +0300

    v2.3

--- university/articles/self_permissions.py
+++ university/articles/self_permissions.py
@@ -1,12 +1,10 @@
 from rest_framework import permissions
 
-# Проверяем, что пользователь аутентифицирован и является ревьюером или администратором
 class IsReviewerOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated and
                 (request.user.is_reviewer or request.user.is_staff or request.user.is_superuser))
 
-# Проверяем, что пользователь аутентифицирован и является модератором или администратором
 class IsStuffOrAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated and
